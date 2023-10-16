1684ecc426928ca6dead145ee14dfcc4fee57bef
Merisbilisim <145693807+Merisbilisim@users.noreply.github.com>
2023-10-16 15:33:00 +0300

    Add files via upload

---
+++ ornek4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ornek4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cmbGun = new System.Windows.Forms.ComboBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbGun
+            // 
+            this.cmbGun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbGun.FormattingEnabled = true;
+            this.cmbGun.Items.AddRange(new object[] {
+            "Pazartesi",
+            "Salı",
+            "Çarşamba",
+            "Perşembe",
+            "Cuma",
+            "Cumartesi",
+            "Pazar"});
+            this.cmbGun.Location = new System.Drawing.Point(46, 136);
+            this.cmbGun.Name = "cmbGun";
+            this.cmbGun.Size = new System.Drawing.Size(175, 24);
+            this.cmbGun.TabIndex = 0;
+            this.cmbGun.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(42, 204);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(51, 20);
+            this.lblSonuc.TabIndex = 1;
+            this.lblSonuc.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(337, 290);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.cmbGun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox cmbGun;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ ornek4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ornek4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string gunad=cmbGun.Text;
+            int gunNo=cmbGun.SelectedIndex;
+            gunNo++;
+            lblSonuc.Text = gunad + "Haftanın" + gunNo + "Gündür";
+
+            if(gunNo==1|| gunNo == 2||gunNo==3)
+            {
+                lblSonuc.Text = "Çıkış Saatleri 16:40";
+            }
+            else if(gunNo==4|| gunNo==5)
+            {
+                lblSonuc.Text = "Çıkış Saatleri 15:50";
+            }
+            else
+            {
+                lblSonuc.Text = "Tatil";
+            }
+        }
+    }
+}
